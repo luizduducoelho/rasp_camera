a27cb0e5a17bf425e3571d30e4a4fe6b2ef27fc6
Luiz Coelho <lelcoelho@yahoo.com.br>
2019-02-18 13:21:21 -0300

    Put in a loop

--- rasp_camera.py
+++ rasp_camera.py
@@ -5,16 +5,18 @@ import numpy as np
 import cv2
 
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # allow the camera to warmup
 sleep(0.1)
 
-# grab an image from the camera
-camera.rotation = 180
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+for i in range(100):
+	# grab an image from the camera
+	camera.rotation = 180
+	camera.capture(rawCapture, format="bgr")
+	image = rawCapture.array
 
-# display the image on screen and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+	# display the image on screen and wait for a keypress
+	cv2.imshow("Image", image)
+	cv2.waitKey(1)
